a31b09f699639768cf99f6949a6ce6cb016d2af9
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-10-31 02:21:08 +0300

    правки ревью 4

--- middlewares/Auth.js
+++ middlewares/Auth.js
@@ -7,7 +7,7 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходимdа авторизация');
+    throw new UnauthorizedError('Необходима авторизация');
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
